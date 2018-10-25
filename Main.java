a6e73f120b0c3809c81a27d89c8ec51776193445
westernmeadow <43891839+westernmeadow@users.noreply.github.com>
2018-10-25 11:25:00 -0700

    12

--- Main.java
+++ Main.java
@@ -229,9 +229,9 @@ class Main
         
         int[] ar2new = new int[ar2.length];
         for (int i=0;i<ar2.length;i++)
-            ar2new[ar2.length-1-i] = ar2[i];
+            ar2new[i] = ar2[i];
         for (int i=0;i<ar2.length;i++)
-            ar2new[i] = ar2[ar2.length-1-i];
+            ar2[i] = ar2new[ar2.length-1-i];
             
         for (int i=0;i<ar2.length;i++)
             System.out.println("ar2["+i+"] = "+ar2[i]); 
@@ -261,6 +261,33 @@ class Main
          */
         String[] ar5 = new String[16];
         ar5[0] = "Four";
+        ar5[1] = "score";
+        ar5[2] = "and";
+        ar5[3] = "seven";
+        ar5[4] = "years";
+        ar5[5] = "ago";
+        ar5[6] = "our";
+        ar5[7] = "fathers";
+        ar5[8] = "brought";
+        ar5[9] = "forth";
+        ar5[10] ="on";
+        ar5[11] = "this";
+        ar5[12] = "continent";
+        ar5[13] = "a";
+        ar5[14] = "new";
+        ar5[15] = "nation";
+        
+        int[] ar6 = new int[ar5.length];
+        for (int i=0;i<ar6.length;i++)
+            ar6[i] = ar5[i].length();
+        
+        k=0;
+        for (int i=0;i<ar6.length;i++)
+        {
+            if (ar6[i]>=5)
+                k++;
+        }
+        System.out.println("There are "+k+" word(s) that contain five or more letters.");
         
         /*
          * Task 13
@@ -276,6 +303,17 @@ class Main
          * Use a for loop to print out the names of monster that start with
          * a vowel
          */
+        String[] monsterArray = new String[5];
+        monsterArray[0] = "Cookie";
+        monsterArray[1] = "Grover";
+        monsterArray[2] = "Oscar the Grouch";
+        monsterArray[3] = "Elmo";
+        monsterArray[4] = "Rosita";
+        
+        for (int i=0;i<monsterArray.length;i++)
+        {
+            if (monsterArray[i].charAt(0));
+        }
         
          /*
           * Task 14
